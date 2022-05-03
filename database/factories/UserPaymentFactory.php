710850551f6d84dcc2c0cd3cf62bf38c4405ba48
JULIOCBL <julio-borgeslopez@outlook.com>
2022-05-03 12:07:51 -0500

    Agregar tabla a la base de datos

---
+++ database/factories/UserPaymentFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\UserPayment;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class UserPaymentFactory extends Factory
+{
+    protected $model = UserPayment::class;
+
+    public function definition(): array
+    {
+    	return [
+    	    //
+    	];
+    }
+}
